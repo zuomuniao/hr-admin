b38ac6d8f276051ada939b0681d4cbbb8c604024
zhengwei <js-class@qq.com>
2022-07-13 17:21:25 +0800

    写登录页面

--- src/icons/index.js
+++ src/icons/index.js
@@ -3,7 +3,10 @@ import SvgIcon from '@/components/SvgIcon'// svg component
 
 // register globally
 Vue.component('svg-icon', SvgIcon)
-
+// 等价于
+// import './svg/404.svg'
+// import './svg/bug.svg'
+// 批量导入svg目录中所有的以svg结尾的文件
 const req = require.context('./svg', false, /\.svg$/)
 const requireAll = requireContext => requireContext.keys().map(requireContext)
 requireAll(req)
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -2,14 +2,19 @@ import Cookies from 'js-cookie'
 
 const TokenKey = 'vue_admin_template_token'
 
-export function getToken() {
+export function getToken () {
   return Cookies.get(TokenKey)
 }
 
-export function setToken(token) {
+export function setToken (token) {
   return Cookies.set(TokenKey, token)
 }
 
-export function removeToken() {
+export function removeToken () {
   return Cookies.remove(TokenKey)
 }
+
+// cookie和本地存储
+// 1. cookie是有有效期，可以设置 locaclStorage永久有效，除非用户自己手动清理
+// 2. cookie兼容性支持ie6 会随着请求头自动发送到服务器
+// 3. cookie 4kb左右 localStorage 5MB
